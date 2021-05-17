c04aeec47995f694b06bf9f2709b5e7300918ca8
Quang Quan <61703734+quangquan4179@users.noreply.github.com>
2021-05-17 19:41:33 +0700

    Update Sound.java

--- src/Sound/Sound.java
+++ src/Sound/Sound.java
@@ -1,7 +1,6 @@
 package Sound;
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 import java.io.EOFException;
 import javax.sound.*;
 import javax.sound.sampled.AudioInputStream;
@@ -9,18 +8,9 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import java.net.URL;
 
 public class Sound {
-//	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-//	Scanner scanner = new Scanner(System.in);
-//	File file = new File("sound/soundGame.wav");
-//	AudioInputStream audioStream= AudioSystem.getAudioInputStream(file);
-//	Clip clip = AudioSystem.getClip();
-//	clip.open(audioStream);
-//	clip.start();
-//	String response = scanner.next();
-//	}
+
 	 public static Clip getSound(){
 	        try {
 	        	File file = new File("sound/soundGame.wav");
